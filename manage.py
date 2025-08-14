cd89da9558a9650ddc898fb27d468e4c8f42d367
sauron <kravshanov97@gmail.com>
2025-08-14 17:06:09 +0500

    added a tkinter func for permission to enter

--- manage.py
+++ manage.py
@@ -3,10 +3,22 @@
 import os
 import sys
 
+import tkinter as tk
+from tkinter import messagebox
+
+def request_permission():
+    root = tk.Tk()
+    root.withdraw()
+    javob = messagebox.askyesno("Ruxsat", "Dasturga kirishga ruxsat berasizmi?")
+    if not javob:
+        sys.exit()
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    request_permission()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
