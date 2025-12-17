1e37e013f00cb3d149fba4dd4b46cbae8b483336
maxsudbekjon <toshpulatovm73@gmail.com>
2025-12-17 15:14:15 +0500

    add single login view

--- config/settings.py
+++ config/settings.py
@@ -257,6 +257,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
