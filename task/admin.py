6ee2c7a37dee1f309738c57e774554b207b4a7ae
maxsudbekjon <toshpulatovm73@gmail.com>
2026-01-17 18:22:03 +0500

    a

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Do, Program, Task, CompleteTask
 
-admin.site.register([Do, Program, Task, CompleteTask])
+@admin.register(CompleteTask)
+class CompleteTaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "task", "completed_at", "spent_time")
+    list_filter = ("completed_at", "user")
+    search_fields = ("task__title", "user__username")
+    ordering = ("-completed_at",)
+    fields = ("user", "task", "completed_at", "spent_time")
+
+
+admin.site.register([Do, Program, Task])
--- task/models/complete_task.py
+++ task/models/complete_task.py
@@ -1,12 +1,12 @@
 from .task import *
+from django.utils import timezone
 
 
 class CompleteTask(models.Model):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    completed_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(default=timezone.now)
     spent_time = models.IntegerField(default=0)  
 
     def __str__(self):
         return str(self.task.title)
-
