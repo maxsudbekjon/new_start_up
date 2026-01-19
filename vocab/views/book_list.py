821f5204b1298ed7199a0f2b56c4f011da1a5163
maxsudbekjon <toshpulatovm73@gmail.com>
2026-01-19 17:19:30 +0500

    a

--- vocab/views/book_list.py
+++ vocab/views/book_list.py
@@ -2,8 +2,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from vocab.models.book import Book
 from vocab.serializers import BookModelSerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Book"])
 class BookListAPIView(generics.ListAPIView):
     queryset=Book.objects.all()
     serializer_class=BookModelSerializer
