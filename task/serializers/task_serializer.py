4e570d9d5875f8f65c6ac10725d04b521aab38fe
maxsudbekjon <toshpulatovm73@gmail.com>
2026-01-19 18:47:55 +0500

    a

--- task/serializers/task_serializer.py
+++ task/serializers/task_serializer.py
@@ -11,41 +11,13 @@ from vocab.models.vocab import Vocab
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    book = serializers.CharField(source="book.name", read_only=True)
-
     class Meta:
         model = Task
         fields = ["title", "program", "count", "book"]
 
         read_only_fields = ['user']
 
-    @staticmethod
-    def _is_book_reading(title):
-        normalized = (title or "").strip().lower()
-        return normalized in {"kitob o'qish", "kitob oqish"}
-
-    def create(self, validated_data):
-        title_obj = validated_data.get("title")
-        if title_obj and self._is_book_reading(title_obj.title):
-            book = Book.objects.order_by("id").first()
-            if not book:
-                raise serializers.ValidationError({"book": "Kitob topilmadi, avval kitob qo'shing."})
-            validated_data["book"] = book
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance = super().update(instance, validated_data)
-        if self._is_book_reading(instance.title.title):
-            if not instance.book:
-                book = Book.objects.order_by("id").first()
-                if not book:
-                    raise serializers.ValidationError({"book": "Kitob topilmadi, avval kitob qo'shing."})
-                instance.book = book
-                instance.save(update_fields=["book"])
-        elif instance.book_id is not None:
-            instance.book = None
-            instance.save(update_fields=["book"])
-        return instance
+    
 
 class ListTaskSerializer(serializers.ModelSerializer):
     title = serializers.CharField(source="title.title", read_only=True)
