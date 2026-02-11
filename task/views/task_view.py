b9df2010427f3d4653bf28ab066dddf6f77a875d
maxsudbekjon <toshpulatovm73@gmail.com>
2026-02-11 15:58:15 +0500

    add new api

--- task/views/task_view.py
+++ task/views/task_view.py
@@ -1,5 +1,5 @@
 from dateutil.utils import today
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
